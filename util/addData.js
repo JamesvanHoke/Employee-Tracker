090b322e40577af8f859ab05550696c012380a55
James V <james.vanhoke@yahoo.com>
2021-03-30 03:21:38 -0700

    Add Employee func

--- util/addData.js
+++ util/addData.js
@@ -90,8 +90,8 @@ const addEmployeeQuestions = [
   {
     type: "list",
     name: "manager",
-    message: "Does this employee have a manager? if they don't leave blank",
-    choices: [], //Add function to pull existing employees.
+    message: "Does this employee have a manager? if they don't select NULL",
+    choices: [], //Function within inquirer prompt will map over this array with our current list of employees from the DB.
   },
 ];
 
@@ -133,35 +133,84 @@ const addDataPrompt = async function () {
       addRoleQuestions[2].choices = currentDepts.map((data) => data.dept_name);
 
       // Runs Inquirer prompt from Role Questions array, pulls the title, salary, and department id into a variable
-      const response = await inquirer.prompt(addRoleQuestions);
+      const roleResponse = await inquirer.prompt(addRoleQuestions);
 
       // Our MySQL prompt to insert new data into our table using placeholders
-      const RoleInsert =
-        "INSERT INTO employeeRole (title, salary, department_id) VALUES (?, ?, ?);";
+      const roleInsert =
+        "INSERT INTO employee_role (title, salary, department_id) VALUES (?, ?, ?);";
 
       // Checks our database for dept name ID's that match the dept we passed in via inquirer
       const deptIDQuery = await connection.query(
         "SELECT id FROM department WHERE dept_name = ?;",
-        [response.department]
+        [roleResponse.department]
       );
 
-      // Maps the returned object so we can pass it into our mysql
+      // Maps the returned object so we can pass it into our mysql statement
       let id = deptIDQuery.map((data) => data.id);
 
       // Puts together our query and sends it along to the database generating a new role.
-      await connection.query(RoleInsert, [response.title, response.salary, id]);
+      await connection.query(roleInsert, [
+        roleResponse.title,
+        roleResponse.salary,
+        id,
+      ]);
 
       // Logs a confirmation to the user so they can see that the role they created has been added to their selected department.
       console.log(
-        `The role of ${response.title} has been added to the ${response.department} department in the database.`
+        `The role of ${roleResponse.title} has been added to the ${roleResponse.department} department in the database.`
       );
       break;
 
     case "Employee":
+      // Grabs our current roles list from the database
+      const currentRoles = await connection.query(
+        "SELECT * FROM employee_role;"
+      );
 
-      // Run Department Questions array through inq
-      // INSERT in employee table, pass in the first name, last name, role ID, and manager id values pulled from prompt resp.
-      // Give user a console.log letting them know we did it
+      // Maps over inquirer list choices with our list of roles
+      addEmployeeQuestions[2].choices = currentRoles.map((data) => data.title);
+
+      // Grabs our current employee list so user can pick the manager
+      const currentManagers = await connection.query(
+        "SELECT CONCAT(first_name, ' ', last_name, ' Employee ID#', id) AS name FROM employee;"
+      );
+
+      // Maps over inquirer choices with our list of employees
+      addEmployeeQuestions[3].choices = currentManagers.map(
+        (data) => data.name
+      );
+      // Pushes a choice of null for employees without a manager.
+      // TODO: rename Null to "No Manager" and have a conversion to null behind the scenes.
+      addEmployeeQuestions[3].choices.push("NULL");
+
+      // Runs Inquirer prompt from Employee Questions array, pulls the first name, last name, manager id, and role id into a variable
+      const employeeResponse = await inquirer.prompt(addEmployeeQuestions);
+
+      const managerIDQuery = await connection.query(
+        "SELECT id FROM employee WHERE CONCAT (first_name, ' ', last_name, '; Employee ID#', id) = ?;",
+        [employeeResponse.manager]
+      );
+      const managerId = await managerIDQuery.map((data) => data.id);
+
+      console.log(managerId);
+
+      //   const roleIdQuery = await connection.query(
+      //     "SELECT id FROM employee_role WHERE title = ?;",
+      //     [employeeResponse.role]);
+      //   const roleId = await roleIdQuery.map((data) => data.id);
+
+      let roleId = 2;
+
+      const employeeInsert =
+        "INSERT INTO employee (first_name, last_name, manager_id, role_id) VALUES (?, ?, ?, ?);";
+
+      await connection.query(employeeInsert, [
+        employeeResponse.first_name,
+        employeeResponse.last_name,
+        managerId,
+        roleId,
+      ]);
+
       break;
   }
 };
