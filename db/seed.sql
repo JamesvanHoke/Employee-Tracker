afff17493f0daeedebd8b2a31446ade004e6a1b0
James V <james.vanhoke@yahoo.com>
2021-03-29 20:46:36 -0700

    DB Seed info

---
+++ db/seed.sql
@@ -0,0 +1,26 @@
+INSERT INTO department (dept_name)
+VALUES 
+("Sales"),
+("Human Resources"),
+("R&D"),
+("Marketing"),
+("Finance"),
+("Admin");
+
+INSERT INTO employeeRole (title, salary, department_id)
+VALUES
+("Accountant", 160000, 5),
+("Regional Manager", 130000, 6),
+("Lead Developr", 120000, 3),
+("Sales Lead", 70000, 1),
+("Social Media Manager", 25000, 4),
+("HR Rep", 61000, 2);
+
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES
+("Lucian", "Beil", 2),
+("Eva", "Wood", 6),
+("Teagan", "Mercado", 5),
+("Julien", "Searle", 4),
+("Arianna", "Thomas", 3),
+("Tomi", "Reyna", 1);
