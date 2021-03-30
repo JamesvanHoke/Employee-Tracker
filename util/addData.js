e5b09a229053516e457b26106f4a3db124ab6d66
James V <james.vanhoke@yahoo.com>
2021-03-29 21:51:10 -0700

    AddRoleQuestions & Validation

--- util/addData.js
+++ util/addData.js
@@ -1,10 +1,12 @@
 // * Add departments, roles, employees
 
+// Question Array for adding a new department
 const addDeptQuestions = [
   {
     type: "input",
-    message: "Please input the name of the department you'd like to add.",
+    message: "What is the name of the department you'd like to add.",
     name: "dept_name",
+    // Validates to make sure the user input a string and not a number
     validate(value) {
       if (isNaN(value) === true) {
         return true;
@@ -14,15 +16,41 @@ const addDeptQuestions = [
   },
 ];
 
+//Question Array for adding a new role
+const addRoleQuestions = [
+  {
+    type: "input",
+    message: "What is the title of the role you'd like to add.",
+    name: "title",
+    // Validates to make sure the user input a string and not a number
+    validate(value) {
+      if (isNaN(value) === true) {
+        return true;
+      }
+      return "please enter a valid title for your role.";
+    },
+  },
+  {
+    type: "input",
+    message: "What is the yearly salary of the role",
+    name: "salary",
+    // Validates to make sure the user input a number and not a string
+    validate(value) {
+      if (isNaN(value) === false) {
+        return true;
+      }
+      return "please enter a valid number for your salary.";
+    },
+  },
+  {
+    type: "list",
+    message: "Which department does this role belong to?",
+    name: "department",
+    choices: [], // Add function to pull from existing departments in our DB and present them
+  },
+];
 
 
-
-// validate(value) {
-//   if (isNaN(value) === false) {
-//     return true;
-//   }
-//   return false;
-// }
 
 // function addFlavor() {
 //   inquirer
