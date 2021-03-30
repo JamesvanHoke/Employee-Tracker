3337ebba74eb57ca991e73a533615cd33acb9bb1
James V <james.vanhoke@yahoo.com>
2021-03-30 01:19:46 -0700

    addRole functionality.

--- util/addData.js
+++ util/addData.js
@@ -1,7 +1,7 @@
 const inquirer = require("inquirer");
 
 // Remove me for production, used to test prompts and adding to database
-const connection = require("./connection")
+const connection = require("./connection");
 
 //---------------------------------------------- Question Arrays ----------------------------------------------
 
@@ -60,24 +60,24 @@ const addEmployeeQuestions = [
   {
     type: "input",
     name: "first_name",
-    message: "What is the employee's first name?", //Add Validation
+    message: "What is the employee's first name?", //TODO: Add Validation
   },
   {
     type: "input",
     name: "last_name",
-    message: "What is the employee's last name?", //Add Validation
+    message: "What is the employee's last name?", //TODO: Add Validation
   },
   {
     type: "list",
     name: "role",
     message: "What role does this employee hold?",
-    choices: [], //Add Function to pull existing roles from DB
+    choices: [], //Function within inquirer prompt will map over this array with our current roles in the DB.
   },
   {
     type: "list",
     name: "manager",
-    message: "Does this employee have a manager?",
-    choices: [], //Add function to pull existing managers from DB
+    message: "Does this employee have a manager? if they don't leave blank",
+    choices: [], //Add function to pull existing employees.
   },
 ];
 
@@ -98,7 +98,7 @@ const addDataPrompt = async function () {
   //   Switch case for our first inq prompt
   switch (SelectedType) {
     case "Department":
-      // Runs Inquirer prompt from Questions array, pulls the dept_name from the response
+      // Runs Inquirer prompt from Questions array, pulls the dept_name from the response into a deconstructed variable
       const { dept_name } = await inquirer.prompt(addDeptQuestions);
 
       //Our MySQL prompt to insert new data into our table using placeholders
@@ -107,16 +107,44 @@ const addDataPrompt = async function () {
       // await function, passes in our response and fills in placeholder with dept_name variable
       await connection.query(DepartmentInsert, [dept_name]);
 
-      //Logs a confirmation log to the user so they can see that something has been done.
+      //Logs a confirmation to the user so they can see that the department has been added.
       console.log(dept_name + " has been added to the database.");
       break;
 
     case "Role":
-      // Run role Questions array through inq
-      // INSERT in role table, pass in the value salary, title, and dept_id pulled from prompt resp.
-      // Give user a console.log letting them know we did it
+      // Query our DB for all current departments and save it to a variable.
+      const currentDepts = await connection.query("SELECT * FROM department;");
+
+      // Targets the third question (second index) of our role questions array, and sets the choices array to be equal to our mapped department db
+      addRoleQuestions[2].choices = currentDepts.map((data) => data.dept_name);
+
+      // Runs Inquirer prompt from Role Questions array, pulls the title, salary, and department id into a variable
+      const response = await inquirer.prompt(addRoleQuestions);
+
+      // Our MySQL prompt to insert new data into our table using placeholders
+      const RoleInsert =
+        "INSERT INTO employeeRole (title, salary, department_id) VALUES (?, ?, ?);";
+
+      // Checks our database for dept name ID's that match the dept we passed in via inquirer
+      const deptIDQuery = await connection.query(
+        "SELECT id FROM department WHERE dept_name = ?;",
+        [response.department]
+      );
+
+      // Maps the returned object so we can pass it into our mysql
+      let id = deptIDQuery.map((data) => data.id);
+
+      // Puts together our query and sends it along to the database generating a new role.
+      await connection.query(RoleInsert, [response.title, response.salary, id]);
+
+      // Logs a confirmation to the user so they can see that the role they created has been added to their selected department.
+      console.log(
+        `The role of ${response.title} has been added to the ${response.department} department in the database.`
+      );
       break;
+
     case "Employee":
+
       // Run Department Questions array through inq
       // INSERT in employee table, pass in the first name, last name, role ID, and manager id values pulled from prompt resp.
       // Give user a console.log letting them know we did it
@@ -126,3 +154,5 @@ const addDataPrompt = async function () {
 
 // Test call, comment out before production.
 addDataPrompt();
+
+exports.module = addDataPrompt;
