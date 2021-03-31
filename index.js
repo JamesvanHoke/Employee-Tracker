c0652c4e046f1c1578428f26b7f27344fd778ed6
James V <james.vanhoke@yahoo.com>
2021-03-30 20:02:13 -0700

    fixed inq prompts + opening ascii art.

--- index.js
+++ index.js
@@ -1,30 +1,71 @@
 // Import our MySQL promisify'd connection from /util
-const connection = require("./util/connection");
 const inquirer = require("inquirer");
-const ConsoleTable = require("console.table");
+const addDataPrompt = require("./util/addData");
+const viewDataPrompt = require("./util/viewData");
+const updateDataPrompt = require("./util/updateData");
+const connection = require("./util/connection");
 
-function init() {
-  inquirer
-    .prompt([
-      {
-        type: "list",
-        name: "whatToDo",
-        message: "What would you like to do?",
-        choices: ["View All Flavors", "Add A Flavor", "Exit"],
-      },
-    ])
-    .then(function (response) {
-      switch (response.whatToDo) {
-        case "View All Flavors":
-          viewFlavors();
-          break;
-        case "Add A Flavor":
-          addFlavor();
-          break;
-        default:
-          connection.end();
-      }
-    });
-}
+console.log(`
+
+/$$$$$$$$                         /$$                                         
+| $$_____/                        | $$                                        
+| $$       /$$$$$$/$$$$   /$$$$$$ | $$  /$$$$$$  /$$   /$$  /$$$$$$   /$$$$$$ 
+| $$$$$   | $$_  $$_  $$ /$$__  $$| $$ /$$__  $$| $$  | $$ /$$__  $$ /$$__  $$
+| $$__/   | $$ \ $$ \ $$| $$  \ $$| $$| $$  \ $$| $$  | $$| $$$$$$$$| $$$$$$$$
+| $$      | $$ | $$ | $$| $$  | $$| $$| $$  | $$| $$  | $$| $$_____/| $$_____/
+| $$$$$$$$| $$ | $$ | $$| $$$$$$$/| $$|  $$$$$$/|  $$$$$$$|  $$$$$$$|  $$$$$$$
+|________/|__/ |__/ |__/| $$____/ |__/ \______/  \____  $$ \_______/ \_______/
+                        | $$                     /$$  | $$                    
+                        | $$                    |  $$$$$$/                    
+                        |__/                     \______/                     
+ /$$$$$$$$                           /$$                                      
+|__  $$__/                          | $$                                      
+   | $$  /$$$$$$  /$$$$$$   /$$$$$$$| $$   /$$  /$$$$$$   /$$$$$$             
+   | $$ /$$__  $$|____  $$ /$$_____/| $$  /$$/ /$$__  $$ /$$__  $$            
+   | $$| $$  \__/ /$$$$$$$| $$      | $$$$$$/ | $$$$$$$$| $$  \__/            
+   | $$| $$      /$$__  $$| $$      | $$_  $$ | $$_____/| $$                  
+   | $$| $$     |  $$$$$$$|  $$$$$$$| $$ \  $$|  $$$$$$$| $$                  
+   |__/|__/      \_______/ \_______/|__/  \__/ \_______/|__/                  
+                                                                              
+                                                                              
+                                                                               
+`);
+
+const init = async function () {
+  let { whatToDo } = await inquirer.prompt([
+    {
+      type: "list",
+      name: "whatToDo",
+      message: "What would you like to do?",
+      choices: [
+        "View The Company",
+        "Add To The Company",
+        "Update The Company",
+        "Exit",
+      ],
+    },
+  ]);
+
+  switch (whatToDo) {
+    case "View The Company":
+      await viewDataPrompt(connection);
+      await init();
+      break;
+    case "Add To The Company":
+      await addDataPrompt(connection);
+      await init();
+      break;
+    case "Update The Company":
+      await updateDataPrompt(connection)
+      await init();
+      break;
+    case "Delete From The Company":
+      console.log("I still need to have a function made");
+      break;
+    default:
+      connection.end();
+      break;
+  }
+};
 
 init();
